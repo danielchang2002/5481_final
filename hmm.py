11240fba45b950018eedc265944dd9a1fc92e976
danielchang2002 <danielchang2002@gmail.com>
2022-12-07 23:12:44 -0600

    :hankey: working but possibly buggy

--- hmm.py
+++ hmm.py
@@ -1,81 +1,71 @@
 import numpy as np
 from collections import defaultdict
-import pdb
+import matplotlib.pyplot as plt
 
-THETA = 0.35
-TEST = True
-pseudocount = 0.01
+THETA = 0.8
+DUMMY = False
+pseudocount = 0.0000001
 
-def main():
-  if TEST:
-    sequences = np.array([
-      ["A", "C", "D", "E", "F", "A", "C", "A", "D", "F",],
-      ["A", "F", "D", "A", "-", "-", "-", "C", "C", "F",],
-      ["A", "-", "-", "E", "F", "D", "-", "F", "D", "C",],
-      ["A", "C", "A", "E", "F", "-", "-", "A", "-", "C",],
-      ["A", "D", "D", "E", "F", "A", "A", "A", "D", "F",],
-    ])
-  else:
-    with open("ADH/ADH_MSA.fasta") as f:
-      sequences = np.array([list(line.strip()) for line in f if not line.startswith(">")])
+class pHMM:
+  def __init__(self, sequences):
+    # find match states: columns that have less than theta proportion of insertions
+    self.match_state = (sequences == "-").mean(axis=0) < THETA
 
-  # find match states: columns that have less than theta proportion of insertions
-  match_state = (sequences == "-").mean(axis=0) < THETA
+    # iterate through sequences, compute their path through HMM, count emission and transitions
 
-  # iterate through sequences, compute their path through HMM, count emission and transitions
+    self.transition_counts = defaultdict(int)
+    self.emission_counts = defaultdict(int)
+    self.state_counts = defaultdict(int)
 
-  transition_counts = defaultdict(int)
-  emission_counts = defaultdict(int)
-  state_counts = defaultdict(int)
+    for seq in sequences:
+      last_state = "s"
+      index = 0
+      self.state_counts[last_state] += 1
+      for col in range(sequences.shape[1]):
+        char = seq[col] != "-"
+        match_col = self.match_state[col]
+        if char and match_col:
+          # match
+          index += 1
+          new_state = f"m{index}"
+          self.transition_counts[(last_state, new_state)] += 1
+          last_state = new_state
+          self.state_counts[new_state] += 1 + pseudocount
+          self.emission_counts[(new_state, seq[col])] += 1
+        elif char and not match_col:
+          # insertion
+          new_state = f"i{index}"
+          self.transition_counts[(last_state, new_state)] += 1
+          last_state = new_state
+          self.state_counts[new_state] += 1 + pseudocount
+          self.emission_counts[(new_state, seq[col])] += 1
+        elif not char and match_col:
+          # deletion
+          index += 1
+          new_state = f"d{index}"
+          self.transition_counts[(last_state, new_state)] += 1
+          last_state = new_state
+          self.state_counts[new_state] += 1 + pseudocount
+      self.transition_counts[(last_state, "e")] += 1
+      self.state_counts["e"] += 1
 
-  for seq in sequences:
-    last_state = "s"
-    index = 0
-    state_counts[last_state] += 1
-    for col in range(sequences.shape[1]):
-      char = seq[col] != "-"
-      match_col = match_state[col]
-      if char and match_col:
-        # match
-        index += 1
-        new_state = f"m{index}"
-        transition_counts[(last_state, new_state)] += 1
-        last_state = new_state
-        state_counts[new_state] += 1
-        emission_counts[(new_state, seq[col])] += 1
-      elif char and not match_col:
-        # insertion
-        new_state = f"i{index}"
-        transition_counts[(last_state, new_state)] += 1
-        last_state = new_state
-        state_counts[new_state] += 1
-        emission_counts[(new_state, seq[col])] += 1
-      elif not char and match_col:
-        # deletion
-        index += 1
-        new_state = f"d{index}"
-        transition_counts[(last_state, new_state)] += 1
-        last_state = new_state
-        state_counts[new_state] += 1
-    transition_counts[(last_state, "e")] += 1
-    state_counts["e"] += 1
+    self.transition_prob = defaultdict(int)
+    self.emission_prob = defaultdict(int)
 
-  transition_prob = defaultdict(int)
-  emission_prob = defaultdict(int)
-  for t in transition_counts:
-    transition_prob[t] = transition_counts[t] / state_counts[t[0]]
-  
-  for e in emission_counts:
-    emission_prob[e] = emission_counts[e] / state_counts[e[0]]
+    for t in self.transition_counts:
+      self.transition_prob[t] = self.transition_counts[t] / self.state_counts[t[0]]
+    
+    for e in self.emission_counts:
+      self.emission_prob[e] = (self.emission_counts[e] + pseudocount) / self.state_counts[e[0]]
 
-  num_match_states = match_state.sum()
 
-  test_seq = "ACAFDEAF"
-
-  # return log prob
-  def score(query_seq):
+  def score(self, query_seq):
+    """
+    Returns log prob 
+    """
     memo = {}
     best_prev = {}
+    num_match_states = self.match_state.sum()
     def score_helper(seq, state):
       # memo case
       if (seq, state) in memo: return memo[(seq, state)]
@@ -85,11 +75,11 @@ def main():
       # end case
       if state == "e":
         prev_state = f"m{num_match_states}"
-        score_dict[prev_state] = score_helper(seq, prev_state) + np.log(transition_prob[(prev_state, state)] + pseudocount)
+        score_dict[prev_state] = score_helper(seq, prev_state) + np.log(self.transition_prob[(prev_state, state)] + pseudocount)
         prev_state = f"i{num_match_states}"
-        score_dict[prev_state] = score_helper(seq, prev_state) + np.log(transition_prob[(prev_state, state)] + pseudocount)
+        score_dict[prev_state] = score_helper(seq, prev_state) + np.log(self.transition_prob[(prev_state, state)] + pseudocount)
         prev_state = f"d{num_match_states}"
-        score_dict[prev_state] = score_helper(seq, prev_state) + np.log(transition_prob[(prev_state, state)] + pseudocount)
+        score_dict[prev_state] = score_helper(seq, prev_state) + np.log(self.transition_prob[(prev_state, state)] + pseudocount)
 
         best_score = -np.inf
         best_prev_state = None
@@ -106,60 +96,60 @@ def main():
       u = int(state[1])
 
       if state[0] == "m":
-        e_p = np.log(emission_prob[(state, seq[-1])] + pseudocount)
+        e_p = np.log(self.emission_prob[(state, seq[-1])] + pseudocount)
 
         # start
         if u == 1 and len(seq) == 1:
           prev_state = "s"
-          score_dict[prev_state] = e_p + np.log(transition_prob[(prev_state, state)])
+          score_dict[prev_state] = e_p + np.log(self.transition_prob[(prev_state, state)])
         
         if len(seq) > 1 and u > 1:
           # match
           prev_state = f"m{u - 1}"
-          score_dict[prev_state] = e_p + score_helper(seq[:-1], prev_state) + np.log(transition_prob[(prev_state, state)] + pseudocount)
+          score_dict[prev_state] = e_p + score_helper(seq[:-1], prev_state) + np.log(self.transition_prob[(prev_state, state)] + pseudocount)
 
         if len(seq) > 1 and u > 0:
           # insert
           prev_state = f"i{u - 1}"
-          score_dict[prev_state] = e_p + score_helper(seq[:-1], prev_state) + np.log(transition_prob[(prev_state, state)] + pseudocount)
+          score_dict[prev_state] = e_p + score_helper(seq[:-1], prev_state) + np.log(self.transition_prob[(prev_state, state)] + pseudocount)
 
         if len(seq) > 1:
           # delete
           prev_state = f"d{u - 1}"
-          score_dict[prev_state] = e_p + score_helper(seq[:-1], prev_state) + np.log(transition_prob[(prev_state, state)] + pseudocount)
+          score_dict[prev_state] = e_p + score_helper(seq[:-1], prev_state) + np.log(self.transition_prob[(prev_state, state)] + pseudocount)
 
       elif state[0] == "i":
-        e_p = np.log(emission_prob[(state, seq[-1])] + pseudocount)
+        e_p = np.log(self.emission_prob[(state, seq[-1])] + pseudocount)
 
         # start
         if u == 0 and len(seq) == 1:
           prev_state = "s"
-          score_dict[prev_state] = e_p + np.log(transition_prob[(prev_state, state)] + pseudocount)
+          score_dict[prev_state] = e_p + np.log(self.transition_prob[(prev_state, state)] + pseudocount)
 
         if len(seq) > 1 and u > 0:
           # match
           prev_state = f"m{u}"
-          score_dict[prev_state] = e_p + score_helper(seq[:-1], prev_state) + np.log(transition_prob[(prev_state, state)] + pseudocount)
+          score_dict[prev_state] = e_p + score_helper(seq[:-1], prev_state) + np.log(self.transition_prob[(prev_state, state)] + pseudocount)
 
           # insert
         if len(seq) > 1 and u >= 0:
           prev_state = f"i{u}"
-          score_dict[prev_state] = e_p + score_helper(seq[:-1], prev_state) + np.log(transition_prob[(prev_state, state)] + pseudocount)
+          score_dict[prev_state] = e_p + score_helper(seq[:-1], prev_state) + np.log(self.transition_prob[(prev_state, state)] + pseudocount)
       elif state[0] == "d":
         # start
         if u == 1 and len(seq) == 0:
           prev_state = "s"
-          score_dict[prev_state] = np.log(transition_prob[(prev_state, state)] + pseudocount)
+          score_dict[prev_state] = np.log(self.transition_prob[(prev_state, state)] + pseudocount)
 
         if seq != "" and u > 1:
           # match
           prev_state = f"m{u - 1}"
-          score_dict[prev_state] = score_helper(seq, prev_state) + np.log(transition_prob[(prev_state, state)] + pseudocount)
+          score_dict[prev_state] = score_helper(seq, prev_state) + np.log(self.transition_prob[(prev_state, state)] + pseudocount)
 
         if u > 1:
           # delete
           prev_state = f"d{u - 1}"
-          score_dict[prev_state] = score_helper(seq, prev_state) + np.log(transition_prob[(prev_state, state)] + pseudocount)
+          score_dict[prev_state] = score_helper(seq, prev_state) + np.log(self.transition_prob[(prev_state, state)] + pseudocount)
       else:
         print("bruh error")
         return
@@ -175,23 +165,56 @@ def main():
       best_prev[(seq, state)] = best_prev_state
 
       return best_score
-
     s = score_helper(query_seq, "e")
-    return s, best_prev
 
-  s, best_prev = score(test_seq)
-  print(s)
+    return s
 
-  curr = "e"
-  seq = test_seq
+def main():
+  with open("ADH/train.fasta") as f:
+    adh_train_sequences = np.array([list(line.strip()) for line in f if not line.startswith(">")])
+  with open("ACDH/train.fasta") as f:
+    acdh_train_sequences = np.array([list(line.strip()) for line in f if not line.startswith(">")])
+  with open("ADH/test.fasta") as f:
+    adh_test_sequences = [line.strip() for line in f if not line.startswith(">")]
+  with open("ACDH/test.fasta") as f:
+    acdh_test_sequences = [line.strip() for line in f if not line.startswith(">")]
 
-  while True:
-    print(seq, curr)
-    if (seq, curr) not in best_prev: break
-    curr = best_prev[(seq, curr)]
-    if curr[0] == "m" or curr[0] == "i":
-      seq = seq[:-1]
+  print(np.mean([len(seq) for seq in adh_train_sequences]))
+  print(np.mean([len(seq) for seq in acdh_train_sequences]))
+  print(np.mean([len(seq) for seq in adh_test_sequences]))
+  print(np.mean([len(seq) for seq in acdh_test_sequences]))
 
+  adh_pHMM = pHMM(adh_train_sequences)
+  acdh_pHMM = pHMM(acdh_train_sequences)
+
+  adh_pHMM_adh = []
+  acdh_pHMM_adh = []
+  adh_pHMM_acdh = []
+  acdh_pHMM_acdh = []
+
+  for adh_test in adh_test_sequences:
+    n = len(adh_test)
+    adh_pHMM_adh.append(adh_pHMM.score(adh_test))
+    acdh_pHMM_adh.append(acdh_pHMM.score(adh_test))
+
+  for acdh_test in acdh_test_sequences:
+    n = len(acdh_test)
+    adh_pHMM_acdh.append(adh_pHMM.score(acdh_test))
+    acdh_pHMM_acdh.append(acdh_pHMM.score(acdh_test))
+
+  plt.hist(adh_pHMM_adh, label="alcohol dehydrogenase")
+  plt.hist(adh_pHMM_acdh, label="acetaldehyde dehydrogenase")
+  plt.xlabel("Log Likelihood of alcohol dehydrogenase Profile HMM on holdout genes")
+  plt.ylabel("Count")
+  plt.legend()
+  plt.show()
+
+  plt.hist(acdh_pHMM_adh, label="alcohol dehydrogenase")
+  plt.hist(acdh_pHMM_acdh, label="acetaldehyde dehydrogenase")
+  plt.xlabel("Log Likelihood of acetaldehyde dehydrogenase Profile HMM on holdout genes")
+  plt.ylabel("Count")
+  plt.legend()
+  plt.show()
 
 if __name__ == "__main__":
   main()
